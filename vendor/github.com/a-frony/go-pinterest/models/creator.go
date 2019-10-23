807ce15bbb3eb3874635784b8a0a99b3e3c5b105
Andrey <andrey@iMac.local>
2019-10-23 20:39:23 +0300

    init

--- vendor/github.com/a-frony/go-pinterest/models/creator.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Creator struct {
-	Url       string `json:"url"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Id        string `json:"id"`
-}
